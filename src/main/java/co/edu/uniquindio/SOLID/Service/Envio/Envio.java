042f78f5478e7048ffbbcdabdf5058cb688b2ad3
Jorge Gaitan Portillo <jorge.gaitan@masiv.com>
2025-10-23 11:34:12 -0500

    Codigo JavaFx

--- src/main/java/co/edu/uniquindio/SOLID/Service/Envio/Envio.java
+++
@@ -1,5 +0,0 @@
-package co.edu.uniquindio.SOLID.Service.Envio;
-
-public interface Envio {
-    public double calcularCostoEnvio();
-}
--- src/main/java/co/edu/uniquindio/SOLID/utils/JFXPaths.java
+++
@@ -1,5 +0,0 @@
-package co.edu.uniquindio.SOLID.utils;
-
-public class JFXPaths {
-    public static final String HOLA_MUNDO = "/holaMundo.fxml";
-}
